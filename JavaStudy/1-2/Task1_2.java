3b181d9a7be5a8cf3d3f2b900529af448d3f55b5
srn0130 <serenak.0130@gmail.com>
2020-07-15 19:13:31 +0900

    課題
    Task1_1〜Task1_4.class

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -13,7 +13,7 @@ public class Task1_2 {
     public static void main(String[] args) {
 
         // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
-        String[] country = {JAPAN, AMERICA, KOREA};
+        String[] country = {"JAPAN", "AMERICA", "KOREA"};
         // ② ①で作成した配列の要素数を出力してください。
         System.out.println(country.length);
         /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
@@ -21,7 +21,7 @@ public class Task1_2 {
          *   2番目の要素に 「もも」 を代入
          *   3番目の要素に 「ぶどう」 を代入
          */
-        String[] strArray = {りんご, もも, ぶどう};
+        String[] strArray = {"りんご", "もも", "ぶどう"};
         // ④ ③で作成した配列の2番目の要素を出力しなさい。
         System.out.println(strArray[1]);
         /* ⑤下記の処理について、何をしているのかコメントを記入してください。
